3b75441b6946d3f90a575276cbf9806605306b57
FIMUXD <fimuxd@gmail.com>
2017-07-31 23:45:07 +0900

    update Plot with DataCenter

---
+++ Plot/Plot/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Plot
+//
+//  Created by Bo-Young PARK on 31/7/2017.
+//  Copyright © 2017 joe. All rights reserved.
+//
+
+import Foundation
+
---
+++ Plot/Plot/HomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HomeViewController.swift
+//  Plot
+//
+//  Created by joe on 2017. 7. 31..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var mainTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let mainTableViewCell:UINib = UINib(nibName: "MainCustomCell", bundle: nil)
+        
+        self.mainTableView.register(mainTableViewCell, forCellReuseIdentifier: "mainCustomCell")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:MainCustomCell = tableView.dequeueReusableCell(withIdentifier: "mainCustomCell", for: indexPath) as! MainCustomCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
