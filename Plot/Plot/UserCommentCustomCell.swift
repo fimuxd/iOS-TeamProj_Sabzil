240db0d45ef687fb1c867cbd3d2524a7ff7ecb13
joeseonmi <jemma3136@gmail.com>
2017-08-01 11:39:34 +0900

    Edit UserCommentCustomCell
    Edit UserCommentCustomCell

---
+++ Plot/Plot/UserCommentCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCommentCustomCell.swift
+//  Plot
+//
+//  Created by joe on 2017. 8. 1..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+
+class UserCommentCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
