1056f9eb42c937954c77b34300637359a6ab4317
FIMUXD <fimuxd@gmail.com>
2017-08-01 12:27:42 +0900

    modify git error

--- Plot/Plot/HomeViewController.swift
+++ Plot/Plot/HomeViewController.swift
@@ -9,21 +9,45 @@
 import UIKit
 
 class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    /*******************************************/
+    // MARK: -  Outlet                         //
+    /*******************************************/
+    
     @IBOutlet weak var mainTableView: UITableView!
+    
+    /*******************************************/
+    // MARK: -  Life Cycle                     //
+    /*******************************************/
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presentLoginVC()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.mainTableView.register(UINib(nibName: "MainCustomCell", bundle: nil), forCellReuseIdentifier: "mainCustomCell")
+        self.mainTableView.register(UINib.init(nibName: "MainCustomCell", bundle: nil), forCellReuseIdentifier: "mainCustomCell")
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    /*******************************************/
+    // MARK: -  Table View                     //
+    /*******************************************/
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:MainCustomCell = tableView.dequeueReusableCell(withIdentifier: "mainCustomCell", for: indexPath) as! MainCustomCell
+        
+        cell.localLabel.text = "서울"
+        cell.mainTitleLabel.text = "메인타이틀 텍스트 전시이름이들어갑니다"
+        cell.exhibitionTerm.text = "2017. 07. 08~ 2017. 08. 09"
+        cell.museumName.text = "디뮤지엄"
         return cell
     }
     
@@ -31,15 +55,25 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return 10
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 174
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailViewController:DetailViewController = storyboard?.instantiateViewController(withIdentifier: "detailViewController") as! DetailViewController
+        self.navigationController?.pushViewController(detailViewController, animated: true)
+    }
+    
 
+    /*******************************************/
+    // MARK: - Func                            //
+    /*******************************************/
+    
+    func presentLoginVC(){
+        if !UserDefaults.standard.bool(forKey: "LoginTest"){
+            let loginVC:LoginViewController = storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            present(loginVC, animated: true, completion: nil)
+        }
+    }
+    
 }
---
+++ Plot/Plot/SignUpViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SignUpViewController.swift
+//  Plot
+//
+//  Created by joe on 2017. 8. 1..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    /*******************************************/
+    // MARK: -  Outlet                         //
+    /*******************************************/
+    
+    @IBAction func clickedDismiss(_ sender: UIButton) {
+        dismissSelf()
+    }
+    
+    @IBAction func clickedSignup(_ sender: UIButton) {
+        //signupRequest
+        dismissSelf()
+    }
+    
+    
+
+    /*******************************************/
+    // MARK: -  LifeCycle                      //
+    /*******************************************/
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*******************************************/
+    // MARK: -  Func                           //
+    /*******************************************/
+    
+    func dismissSelf(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
