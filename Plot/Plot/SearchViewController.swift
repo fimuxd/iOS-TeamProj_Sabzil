91d1633f4dd4dc6c379719a367339a5737ae4dff
joeseonmi <jemma3136@gmail.com>
2017-08-02 01:42:32 +0900

    SearchTab 진행중
    새로운 테이블뷰를 띄우려니 검색바에서 하는일을 새로운 테이블뷰가알아야되고, 그냥 그 페이지에서 띄우려니 복잡해질것같고 곰인곰인중입니당

--- Plot/Plot/SearchViewController.swift
+++ Plot/Plot/SearchViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UISearchBarDelegate {
     
     /*******************************************/
-    // MARK: -  Outlet                         //
+    // MARK: -  Outlet & Property              //
     /*******************************************/
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var rankingTableView: UITableView!
-    
+    let sectionTitles:[String] = ["좋아요 랭킹","별점 랭킹","딴거"]
+
     
     /*******************************************/
     // MARK: -  LifeCycle                      //
@@ -23,7 +25,8 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.searchBar.delegate = self
+        searchBar.placeholder = "전시명, 지역, 장르 등 검색"
         self.rankingTableView.dataSource = self
         self.rankingTableView.delegate = self
         self.rankingTableView.register(UINib(nibName: "RankListCustomCell", bundle: nil), forCellReuseIdentifier: "RankListCustomCell")
@@ -33,6 +36,30 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /*******************************************/
+    // MARK: -  Search Bar                     //
+    /*******************************************/
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("서치바 눌려뜸 / 새로운 테이블뷰 모달되야되는데 서치바는 가만히있어야댐")
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("검색된 작품뷰로 이동")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("요걸로 검색됨 \(searchText)")
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("다시 랭킹뷰로 이동")
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        view.endEditing(true)
     }
     
     /*******************************************/
@@ -52,21 +79,26 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return 3
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 0:
-            return "좋아요 랭킹"
-        case 1:
-            return "별점 랭킹"
-        case 2:
-            return "다른등등 랭킹"
-        default:
-            return "알수 없는 값"
-        }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let header = Bundle.main.loadNibNamed("Sectionheader", owner: self, options: nil)?.first as! Sectionheader
+        header.sectionTitleLabel.text = sectionTitles[section]
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section == 2 {
+            return 55
+        }
+        return 4
     }
 
 
---
+++ Plot/Plot/Sectionheader.swift
@@ -0,0 +1,25 @@
+//
+//  Sectionheader.swift
+//  Plot
+//
+//  Created by joe on 2017. 8. 2..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+
+class Sectionheader: UITableViewCell {
+    @IBOutlet weak var sectionTitleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
