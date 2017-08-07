c685ac91075b965aea49ee70e1a7ba1545ec928d
joeseonmi <jemma3136@gmail.com>
2017-08-07 13:43:23 +0900

    myPage 수정중
    CollectionView 추가중

--- Plot/Plot/MyPageViewController.swift
+++ Plot/Plot/MyPageViewController.swift
@@ -8,28 +8,73 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+class MyPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewFlowLayout {
+    
+    /*******************************************/
+    // MARK: -  Outlet                         //
+    /*******************************************/
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var likeExhi:[String] = []
+    var likeGenre:[String] = []
+    
+    
+    
+    /*******************************************/
+    // MARK: -  Life Cycle                     //
+    /*******************************************/
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        self.collectionView.register(UINib(nibName: "RankingCustomCell", bundle: nil), forCellWithReuseIdentifier: "RankingCustomCell")
+        self.collectionView.register(UINib(nibName: "TagCustomCell", bundle: nil), forCellWithReuseIdentifier: "TagCustomCell")
+        self.collectionView.register(UINib(nibName: "CollectionViewHeader", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "CollectionViewHeader")
+        //tag레지스터해야됨
+        
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    /*******************************************/
+    // MARK: -  CollectionView                 //
+    /*******************************************/
+    
+    //첫번째 섹션은 포스터만 들어가있어야되고 두번째 섹션으 태그뷰
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if indexPath.section == 0 {
+            let cell:RankingCustomCell = collectionView.dequeueReusableCell(withReuseIdentifier: "RankingCustomCell", for: indexPath) as! RankingCustomCell
+            cell.rankImage.isHidden = true
+            return cell
+        } else {
+            let cell:TagCustomCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagCustomCell", for: indexPath) as! TagCustomCell
+            
+            return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        <#code#>
+    }
+    
+    
 
 }
