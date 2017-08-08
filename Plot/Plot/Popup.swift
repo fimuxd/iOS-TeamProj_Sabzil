fb32c547ecdaa674d2273417885b210c2e0bc866
joeseonmi <jemma3136@gmail.com>
2017-08-09 03:58:43 +0900

    팝업 추가
    팝업뷰를 추가했는데 거의 하드코딩수준이에요,
    나중에 조교쌤이 했던 방법대로 NIB을 레지스터해서 쓰는 방법으로
    바꿔야 할것 같아요

---
+++ Plot/Plot/Popup.swift
@@ -0,0 +1,61 @@
+//
+//  Popup.swift
+//  Plot
+//
+//  Created by joe on 2017. 8. 8..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+
+class Popup: UIViewController {
+    
+    /*******************************************/
+    // MARK: -  Outlet & Property              //
+    /*******************************************/
+    
+    var senderTag:Int!
+    
+    @IBAction func tappedBlackOut(_ sender: UITapGestureRecognizer) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var starPointPopup: UIView!
+    @IBOutlet weak var commentPopup: UIView!
+
+    @IBOutlet weak var commentViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var starPointBottomConstranint: NSLayoutConstraint!
+    
+    
+    /*******************************************/
+    // MARK: -  LifeCycle                      //
+    /*******************************************/
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        if senderTag == 1 {
+            commentPopup.isHidden = true
+            starPointBottomConstranint.constant = 0
+            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
+            
+        }else if senderTag == 2 {
+            starPointPopup.isHidden = true
+            commentViewBottomConstraint.constant = 0
+            UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
+        }
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+     
+    }
+    
+
+}
